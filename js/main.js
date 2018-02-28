7625e578ff4f6c0e81db9a6f7aaf4c5a10257a24
wenhao <wenhao@freeloops.local>
2018-02-28 23:33:52 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length===2){
-	if(cardsInPlay[0]===cardsInPlay[1]){
+function checkForMatch(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 	}
 	else{
 		alert("Sorry, try again.")
 	}
+
 }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length===2){
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
